001a8e5f5ec523983e9ae561ca33db97329b8747
Steffi Vengels <vengels.stefanie@gmail.com>
2026-08-18 16:42:18 +0200

    add Tag function to Products

--- src/products/models.py
+++ src/products/models.py
@@ -22,12 +22,23 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Product(models.Model):
 
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
     description = models.TextField(max_length=250, null=True, blank=True)
     image = models.ImageField(upload_to="imgs/products/", null=True, blank=True)
     name = models.CharField(max_length=80, blank=False, null=False)
+    tags = models.ManyToManyField(Tag, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))])
 
     created_at = models.DateTimeField(auto_now_add=True)
